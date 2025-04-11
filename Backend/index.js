ca2297fd82ce0cc4a406537b6da5f4da672b74b4
Karthikraut <karthikraut2@gmail.com>
2025-04-11 18:14:09 +0530

    Initial setup has been done and Model has been created

---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+// Importing required modules
+import express from "express";               // Express is a Node.js web framework used to create APIs and web servers.
+import dotenv from "dotenv";                 // dotenv loads environment variables from a .env file into process.env
+import cors from "cors";                     // CORS (Cross-Origin Resource Sharing) middleware to allow/disallow requests from other origins
+import cookieParser from "cookie-parser";    // Parses cookies attached to the client request object
+import connectDB from "./utils/db.js";
+
+// Create an instance of an Express application
+const app = express();
+
+// Load environment variables from a .env file into process.env
+dotenv.config();
+
+// Get the PORT value from environment variables
+const PORT = process.env.PORT;
+
+// Middleware to parse incoming JSON data in request body
+app.use(express.json());
+
+// Middleware to parse URL-encoded data (usually from form submissions)
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse cookies in incoming requests
+app.use(cookieParser());
+
+/*
+ *  CORS configuration object:
+ * - origin: defines which client origins are allowed to access the server.
+ * - credentials: allows cookies and authorization headers to be sent in cross-origin requests.
+ */
+const corsOption = {
+    origin: 'http://localhost:5173', // Only allow this origin (typically the frontend development server)
+    credentials: true                // Allow credentials such as cookies, authorization headers
+};
+
+// Enable CORS using the options defined above
+app.use(cors(corsOption));
+
+
+// Start the server and listen on the defined PORT
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server Running at Port: ", PORT); // Log a message when the server starts successfully
+});
